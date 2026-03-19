390146338ce0aff9e1bdbca772e050f69b62730d
mbasha-dev <mahaboobbasha1302@gmail.com>
2026-03-19 21:05:45 +0530

    Time: 11 ms (88.65%) | Memory: 47.6 MB (12.91%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c : s.toCharArray()) {
+            int len = sb.length();
+            
+            if (len > 0 && sb.charAt(len - 1) == c) {
+                sb.deleteCharAt(len - 1); // remove duplicate
+            } else {
+                sb.append(c); // add character
+            }
+        }
+        
+        return sb.toString();
+    }
+}
