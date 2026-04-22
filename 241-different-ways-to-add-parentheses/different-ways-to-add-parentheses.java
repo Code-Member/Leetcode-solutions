e15358ffe9fdd44f792e9318361909e99950f8a9
mbasha-dev <mahaboobbasha1302@gmail.com>
2026-04-22 21:55:13 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (94.04%) - LeetSync

---
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> diffWaysToCompute(String expression) {
+        return helper(expression, new HashMap<>());
+    }
+
+    private List<Integer> helper(String expr, Map<String, List<Integer>> memo) {
+        if (memo.containsKey(expr)) {
+            return memo.get(expr);
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < expr.length(); i++) {
+            char c = expr.charAt(i);
+
+            if (c == '+' || c == '-' || c == '*') {
+                List<Integer> left = helper(expr.substring(0, i), memo);
+                List<Integer> right = helper(expr.substring(i + 1), memo);
+
+                for (int l : left) {
+                    for (int r : right) {
+                        if (c == '+') {
+                            result.add(l + r);
+                        } else if (c == '-') {
+                            result.add(l - r);
+                        } else {
+                            result.add(l * r);
+                        }
+                    }
+                }
+            }
+        }
+
+        // base case: no operator → number
+        if (result.isEmpty()) {
+            result.add(Integer.parseInt(expr));
+        }
+
+        memo.put(expr, result);
+        return result;
+    }
+}
